57d7dae307d33bbc59ad0d671e5fe687cabda6ce
Andre Berezin <andre.berezin@ut.ee>
2024-04-12 18:46:55 +0300

    quick commit

--- main.go
+++ main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
+
+	// 1. Greet the user
 	fmt.Println("Welcome to the Cypher tool!")
+
+	// 2. Allow the user to select the operation (encrypt or decrypt)
+
+
+	// 3. Allow the user to select the encryption type
+
+
+	// 4. Allow the user to input the message to encrypt/decrypt
+
+
+	// 5. Output the result of the operation
+	// If user input is invalid, the program should keep prompting the user to input again, until valid input is provided. Before validatid the input, it has to be trimmed (remove whitespaces from the beginning and the end of the input).
+	// When encrypting or decrypting, ensure that any non-alphabet characters in the message are left unchanged.
 
 }
