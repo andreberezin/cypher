1eddcb2bea510510dd6389d032e19f117f5587c8
partelpilv <pilv.paertel@gmail.com>
2024-04-13 13:29:21 +0300

    reverse.go

--- reverse.go
+++ reverse.go
@@ -5,15 +5,21 @@ import "strings"
 func Reverse_encrypt(s string) string {
 	var result strings.Builder
 
+	// Iterate over each character in the input string
 	for _, char := range s {
+		// Check if the character is a lowercase letter
 		if char >= 'a' && char <= 'z' {
+			// Reverse the letter's position in the alphabet and append to the result
 			result.WriteByte(byte('z' - (char - 'a')))
-		} else if char >= 'A' && char <= 'Z' {
+		} else if char >= 'A' && char <= 'Z' { // Check if the character is an uppercase letter
+			// Reverse the letter's position in the alphabet and append to the result
 			result.WriteByte(byte('Z' - (char - 'A')))
-		} else {
+		} else { // If the character is not a letter
+			// Append the character unchanged to the result
 			result.WriteByte(byte(char))
 		}
 	}
 
+	// Convert the result builder to a string and return
 	return result.String()
 }
