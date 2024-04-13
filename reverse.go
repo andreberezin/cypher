099e607379626032dc5a9367d7995a9af8ac7c9d
partelpilv <pilv.paertel@gmail.com>
2024-04-13 11:52:37 +0300

    reverse.go

--- reverse.go
+++ reverse.go
@@ -1,13 +1,13 @@
 package main
 
-func Reverse_encrypt(s string) string{
+func Reverse_encrypt(s string) string {
 	var result string = "" // you can rename the variable
-	
+
 	return result
 }
 
-func Reverse_decrypt(s string) string{
+func Reverse_decrypt(s string) string {
 	var result string = "" // you can rename the variable
-	
+
 	return result
 }
