da999f93eac0afde86cf62c1bf5f9cd12a1bad92
partelpilv <pilv.paertel@gmail.com>
2024-04-13 13:54:36 +0300

    custom_encrypt.go

--- custom_encrypt.go
+++ custom_encrypt.go
@@ -1,13 +1,24 @@
-package main
+package cypher
 
-func Custom_encrypt(s string) string {
-	var result string = "" // you can rename the variable
-	
-	return result
+import "strings"
+
+// Custom encryption using Simple Caesar Cipher
+func encryptSimpleCaesar(s string, shift int) string {
+	var result strings.Builder
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' {
+			result.WriteByte((char-'a'+byte(shift))%26 + 'a')
+		} else if char >= 'A' && char <= 'Z' {
+			result.WriteByte((char-'A'+byte(shift))%26 + 'A')
+		} else {
+			result.WriteByte(byte(char))
+		}
+	}
+	return result.String()
 }
 
-func Custom_decrypt(s string) string {
-	var result string = "" // you can rename the variable
-	
-	return result
+// Custom decryption for Simple Caesar Cipher
+func decryptSimpleCaesar(s string, shift int) string {
+	// To decrypt, we shift in the opposite direction, so we negate the shift
+	return encryptSimpleCaesar(s, -shift)
 }
