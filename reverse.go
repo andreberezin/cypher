123e82fee3fe835714fa53cb77ebebbafb753748
partelpilv <pilv.paertel@gmail.com>
2024-04-14 11:50:17 +0300

    reverse.go

--- reverse.go
+++ reverse.go
@@ -2,24 +2,22 @@ package cypher
 
 import "strings"
 
-func Reverse_encrypt(s string) string {
+//This is the reverse encyrption/decription function
+
+func encryptreverse(message string) string {
 	var result strings.Builder
-
-	// Iterate over each character in the input string
-	for _, char := range s {
-		// Check if the character is a lowercase letter
+	for _, char := range message {
 		if char >= 'a' && char <= 'z' {
-			// Reverse the letter's position in the alphabet and append to the result
-			result.WriteByte(byte('z' - (char - 'a')))
-		} else if char >= 'A' && char <= 'Z' { // Check if the character is an uppercase letter
-			// Reverse the letter's position in the alphabet and append to the result
-			result.WriteByte(byte('Z' - (char - 'A')))
-		} else { // If the character is not a letter
-			// Append the character unchanged to the result
-			result.WriteByte(byte(char))
+			x := char - 'a'
+			result.WriteRune('z' - x)
+		} else if char >= 'A' && char <= 'Z' {
+			y := char - 'A'
+			result.WriteRune('Z' - y)
+		} else {
+			result.WriteRune(char)
 		}
-	}
 
-	// Convert the result builder to a string and return
+	}
 	return result.String()
+
 }
