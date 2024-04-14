1804438aef78fbc2edcc49046ffeb5e14fd4cf5e
Andre Berezin <andre.berezin@ut.ee>
2024-04-14 14:55:43 +0300

    validate shift input

--- input.go
+++ input.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func getAndValidateInput() (toEncrypt bool, encoding string, message string, shift int) {
+	
+	
 	// Allow the user to select the operation (encrypt or decrypt)
 	toEncrypt_string := ""
 	TOENCRYPT: fmt.Printf("Select operation: \n 1. Encrypt\n 2. Decrypt\n")
@@ -35,28 +37,36 @@ func getAndValidateInput() (toEncrypt bool, encoding string, message string, shi
 	encoding = strings.ToLower(strings.TrimSpace(encoding)) 
 	
 
+	var shiftInput string
 	// check if input is valid, else ask for input again
 	switch encoding {
 	case "rot13", "1", "reverse", "2":
 		break
-	case "custom", "3": // if user chooses custom then ask for shift number
-		SHIFT: fmt.Printf("\nEnter shift number:\n")
-		fmt.Scan(&shift)
-			
-		if !isNumeric(shift) {
-            fmt.Println("Invalid input. Please enter a number.")
-            goto SHIFT
-        } 
+	case "custom", "3": // if user chooses custom then ask for shift number and validate the input
+		for {
+            fmt.Printf("\nEnter shift key: ")
+            reader := bufio.NewReader(os.Stdin)
+            shiftInput, _ = reader.ReadString('\n')
+            shiftInput = strings.TrimSpace(shiftInput)
+            if !isNumeric(shiftInput) {
+                fmt.Printf("\nInvalid input, please try again.\n")
+				continue
+            }
+            shift, _ = strconv.Atoi(shiftInput) // Convert shiftInput to iteger
+            break
+        }
+
 	default:
 		fmt.Printf("\nInvalid input, enter again.\n")
 		goto ENCODING
-}
+    }
+
 
 	// Allow the user to input the message to encrypt/decrypt
 	reader := bufio.NewReader(os.Stdin) // get user input with bufio to include whitespaces
 	fmt.Printf("\nEnter the message:\n")
-	shiftInput, _ := reader.ReadString('\n')
-	message = strings.TrimSpace(shiftInput)  // remove leading and trailing whitespace
+	message, _ = reader.ReadString('\n')
+	message = strings.TrimSpace(message)  // remove leading and trailing whitespace
 
 	// return the result of the operation
 	return toEncrypt, encoding, message, shift
